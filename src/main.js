64a61824349969d5789cab65dcb8d794fd32e0e9
jangsumin <jsm0726@naver.com>
2023-05-15 16:28:17 +0900

    타입 지정한 API를 사용하여 게시 기능 활성화

--- src/main.js
+++ src/main.js
@@ -11,28 +11,6 @@
 
 const http = require('http')
 
-// JSDoc으로 타입 정의
-/**
- * @typedef Post
- * @property {string} id
- * @property {string} title
- * @property {string} content
- */
-
-/** @type {Post[]} */
-const posts = [
-  {
-    id: 'my_first_post',
-    title: 'My first post',
-    content: 'Hello!',
-  },
-  {
-    id: 'my_second_post',
-    title: 'My second post',
-    content: 'Second post!',
-  },
-]
-
 /**
  * Post
  *
@@ -41,61 +19,60 @@ const posts = [
  * POST /posts
  */
 
+const { routes } = require('./api')
+const { rejects } = require('assert')
+
 const server = http.createServer((req, res) => {
-  // 소괄호를 사용해서 정규식에서 캡쳐 그룹 기능으로 url에서 원하는 부분만 추출 가능
-  const POSTS_ID_REGEX = /^\/posts\/([a-zA-Z0-9-_]+)$/
-  const postIdRegexResult =
-    (req.url && POSTS_ID_REGEX.exec(req.url)) || undefined
+  async function main() {
+    const route = routes.find(
+      (_route) =>
+        req.url &&
+        req.method &&
+        _route.url.test(req.url) &&
+        _route.method === req.method
+    )
 
-  if (req.url === '/posts' && req.method === 'GET') {
-    const result = {
-      posts: posts.map((post) => ({
-        id: post.id,
-        title: post.title,
-      })),
-      totalCount: posts.length,
-    }
-
-    res.statusCode = 200
-    // 브라우저에서 파싱한 결과를 나타냄
-    res.setHeader('Content-Type', 'application/json; charset=utf-8')
-    res.end(JSON.stringify(result))
-  } else if (postIdRegexResult && req.method === 'GET') {
-    // GET /posts/:id
-    const postId = postIdRegexResult[1]
-    const post = posts.find((_post) => _post.id === postId)
-
-    if (post) {
-      res.statusCode = 200
-      res.setHeader('Content-Type', 'application/json; charset=utf-8')
-      res.end(JSON.stringify(post))
-    } else {
+    if (!req.url || !route) {
       res.statusCode = 404
-      res.end('Post not found.')
+      res.end('Not found.')
+      return
     }
-  } else if (req.url === '/posts' && req.method === 'POST') {
-    // 아래 코드 작성하지 않으면 버퍼 형태의 데이터가 돌아 옴
-    req.setEncoding('utf-8')
-    req.on('data', (data) => {
-      /** @typedef CreatePostBody
-       * @property {string} title
-       * @property {string} content
-       */
 
-      /** @type {CreatePostBody} */
-      const body = JSON.parse(data)
-      posts.push({
-        id: body.title.toLowerCase().replace(/\s/g, '_'),
-        title: body.title,
-        content: body.content,
-      })
-    })
-    res.statusCode = 200
-    res.end('Creating post')
-  } else {
-    res.statusCode = 404
-    res.end('Not found.')
+    const regexResult = route.url.exec(req.url)
+
+    if (!regexResult) {
+      res.statusCode = 404
+      res.end('Not found.')
+      return
+    }
+
+    /** @type {Object.<string, *> | undefined} */
+    const reqBody =
+      (req.headers['content-type'] === 'application/json' &&
+        (await new Promise((resolve) => {
+          req.setEncoding('utf-8')
+          req.on('data', (data) => {
+            try {
+              resolve(JSON.parse(data))
+            } catch {
+              throw new Error('Ill-formed json')
+            }
+          })
+        }))) ||
+      undefined
+
+    const result = await route.callback(regexResult, reqBody)
+    res.statusCode = result.statusCode
+
+    if (typeof result.body === 'string') {
+      res.end(result.body)
+    } else {
+      res.setHeader('Content-Type', 'application/json; charset=utf-8')
+      res.end(JSON.stringify(result.body))
+    }
   }
+
+  main()
 })
 
 const PORT = 4000
